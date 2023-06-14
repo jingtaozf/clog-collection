896f825385e60a06f5d4919ee10d1db78e513af0
Jingtao Xu <jingtaozf@gmail.com>
2023-06-14 21:37:01 +0800

    First version of codemirror editor with a simple demo.

--- app.js
+++ app.js
@@ -8,5 +8,12 @@ window.cytoscape = cytoscape;
 import dagre from 'cytoscape-dagre';
 cytoscape.use( dagre );
 
-import * as codemirror from 'codemirror';
-window.codemirror = codemirror;
+import * as CodeMirror from 'codemirror';
+window.CodeMirror = Object.assign({}, CodeMirror);
+
+import * as javascript from "@codemirror/lang-javascript";
+window.CodeMirror.javascript = javascript;
+
+import {EditorState, Compartment} from "@codemirror/state"
+window.CodeMirror.EditorState = EditorState;
+window.CodeMirror.Compartment = Compartment;
